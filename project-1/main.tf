d987ce3d2a1f50e63554ebc4859ce01f74c2887f
Jonathan Dyallo <emeraldusk@gmail.com>
2023-06-05 00:31:39 -0300

    feat(arq): wip arquitecture

--- project-1/main.tf
+++ project-1/main.tf
@@ -4,30 +4,74 @@ terraform {
       source  = "kreuzwerker/docker"
       version = "~> 3.0.1"
     }
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
   }
+
+  required_version = ">= 1.2.0"
 }
 
 provider "docker" {}
 
-resource "docker_image" "dolar-hoy-api" {
-  name         = "dyallo/dolar-hoy-api:latest"
-  keep_locally = false
+provider "aws" {
+  region  = "us-west-2"
 }
 
-resource "docker_container" "dolar-hoy-api" {
-  image = docker_image.dolar-hoy-api.name
-  name  = "dolar-hoy-api"
-  ports {
-    internal = var.PORT
-    external = var.PORT
+resource "aws_instance" "dyallab" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "dyallab-instance"
   }
 
-  env = [
-    "HOST=${var.HOST}",
-    "USERNAME=${var.USERNAME}",
-    "PASSWORD=${var.PASSWORD}",
-    "DATABASE=${var.DATABASE}",
-    "MANUAL_USERNAME=${var.MANUAL_USERNAME}",
-    "MANUAL_PASSWORD=${var.MANUAL_PASSWORD}",
-  ]
+  provisioner "remote-exec" {
+    inline = [
+      "docker pull dyallo/dolar-hoy-api:latest",
+      "docker run -d --name dolar-hoy-api -p ${var.PORT}:${var.PORT} -e HOST=${var.HOST} -e USERNAME=${var.USERNAME} -e PASSWORD=${var.PASSWORD} -e DATABASE=${var.DATABASE} -e MANUAL_USERNAME=${var.MANUAL_USERNAME} -e MANUAL_PASSWORD=${var.MANUAL_PASSWORD} dyallo/dolar-hoy-api:latest",
+    ]
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /scripts/install-nginx.sh",
+      "sh /scripts/install-nginx.sh",
+      "sudo systemctl start nginx"
+    ]
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "cat <<EOF > /etc/nginx/sites-available/dolar.jonathan.com.ar",
+      "server {",
+      "    server_name dolar.jonathan.com.ar;",
+      "",
+      "    location / {",
+      "        proxy_pass http://localhost:4500;",
+      "        proxy_http_version 1.1;",
+      "        proxy_set_header Upgrade $http_upgrade;",
+      "        proxy_set_header Connection 'upgrade';",
+      "        proxy_set_header Host $host;",
+      "        proxy_cache_bypass $http_upgrade;",
+      "    }",
+      "}",
+      "EOF",
+      "ln -s /etc/nginx/sites-available/dolar.jonathan.com.ar /etc/nginx/sites-enabled/dolar.jonathan.com.ar",
+      "nginx -s reload",
+    ]
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /scripts/install-certbot.sh",
+      "sh /scripts/install-certbot.sh",
+      "certbot --nginx -n -d dolar.jonathan.com.ar --agree-tos --email contacto@jonathan.com.ar --no-eff-email"
+    ]
+  }
 }
+
+
+
